2ab944b7ab5f079780a9168858a7f163b2323147
AhmedAbdulla22 <ahmeddev452@gmail.com>
2026-06-07 06:57:02 +0300

    fix css path

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Geist, Geist_Mono, Tajawal } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import { LanguageProvider } from "@/components/providers/language-provider"
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import "./globals.css";
+import "@/app/globals.css";
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
